4e9a364296b1344f5122c9551cf61432e4e40e25
Woohyun Kim <woohyun.kim@hakko-dev.com>
2019-09-30 17:29:50 +0900

    8ㅓㅗㅕㅛㅜㅗㅕㅠㅕㅑㅝㅗ

--- gif_maker.js
+++ gif_maker.js
@@ -42,22 +42,11 @@ module.exports = {
         // start the gif encoder
         this.encode(timeResult, cb);
     },
-    /**
-     * Limit a value between a min / max
-     * @link http://stackoverflow.com/questions/11409895/whats-the-most-elegant-way-to-cap-a-number-to-a-segment
-     * @param number - input number
-     * @param min - minimum value number can have
-     * @param max - maximum value number can have
-     * @returns {number}
-     */
+
     clamp: function(number, min, max){
         return Math.max(min, Math.min(number, max));
     },
-    /**
-     * Calculate the diffeence between timeString and current time
-     * @param {string} timeString
-     * @returns {string|Object} - return either the date passed string, or a valid moment duration object
-     */
+
     time: function (timeString) {
         // grab the current and target time
         let target = moment(timeString);
@@ -74,121 +63,102 @@ module.exports = {
             return moment.duration(difference);
         }
     },
-    /**
-     * Encode the GIF with the information provided by the time function
-     * @param {string|Object} timeResult - either the date passed string, or a valid moment duration object
-     * @param {requestCallback} cb - the callback to be run once complete
-     */
+
     encode: function(timeResult, cb){
         let enc = this.encoder;
         let ctx = this.ctx;
         let tmpDir = '/tmp/'
 
-        // create the tmp directory if it doesn't exist
         if (!fs.existsSync(tmpDir)){
             fs.mkdirSync(tmpDir);
         }
         
         let filePath = tmpDir + this.name + '.gif';
-        
-        // pipe the image to the filesystem to be written
+
         let imageStream = enc
                 .createReadStream()
                     .pipe(fs.createWriteStream(filePath));
-        // once finised, generate or serve
+
         imageStream.on('finish', () => {
-            // only execute callback if it is a function
             typeof cb === 'function' && cb(filePath);
         });
-        
-        // estimate the font size based on the provided width
-        let fontSize = Math.floor(this.width / 12) + 'px';
-        let fontFamily = 'Courier New'; // monospace works slightly better
-        
-        // set the font style
-        ctx.font = [fontSize, fontFamily].join(' ');
+
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
 
-        // start encoding gif with following settings
         enc.start();
         enc.setRepeat(0);
         enc.setDelay(1000);
         enc.setQuality(10);
 
-        // if we have a moment duration object
         if(typeof timeResult === 'object'){
             for(let i = 0; i < this.frames; i++){
-                // extract the information we need from the duration
                 let days = Math.floor(timeResult.asDays());
                 let hours = Math.floor(timeResult.asHours() - (days * 24));
                 let minutes = Math.floor(timeResult.asMinutes()) - (days * 24 * 60) - (hours * 60);
                 let seconds = Math.floor(timeResult.asSeconds()) - (days * 24 * 60 * 60) - (hours * 60 * 60) - (minutes * 60);
                 
-                // make sure we have at least 2 characters in the string
                 days = (days.toString().length == 1) ? '0' + days : days;
                 hours = (hours.toString().length == 1) ? '0' + hours : hours;
                 minutes = (minutes.toString().length == 1) ? '0' + minutes : minutes;
                 seconds = (seconds.toString().length == 1) ? '0' + seconds : seconds;
                 
-                // build the date string
                 let string = [days, '일 ', hours, '시간 ', minutes, '분 ', seconds, '초'].join('');
                 
-                // paint BG
                 ctx.fillStyle = '#221900';
                 ctx.fillRect(0, 0, this.width, this.height);
 
                 const numberFont = 'normal normal bold 30px arial'
                 const timeFont = 'normal normal bold 20px arial'
-                const titleFont = 'normal normal bold 12px arial'
-                const buttonFont = 'normal normal bold 12px arial'
+                const titleFont = 'normal normal bold 18px arial'
+                const buttonFont = 'normal normal bold 20px arial'
                 // 일 숫자
                 ctx.font = numberFont
                 ctx.fillStyle = this.textColor;
-                ctx.fillText(days.toString(), 176/2, 140/2)
+                ctx.fillText(days.toString(), 176/2, 130/2)
                 // 시간 숫자
                 ctx.font = numberFont
                 ctx.fillStyle = this.textColor;
-                ctx.fillText(hours.toString(), 328/2, 140/2)
+                ctx.fillText(hours.toString(), 328/2, 130/2)
                 // 분 숫자
                 ctx.font = numberFont
                 ctx.fillStyle = this.textColor;
-                ctx.fillText(minutes.toString(), 518/2, 140/2)
+                ctx.fillText(minutes.toString(), 518/2, 130/2)
                 // 초 숫자
                 ctx.font = numberFont
                 ctx.fillStyle = this.textColor;
-                ctx.fillText(seconds.toString(), 671/2, 140/2)
+                ctx.fillText(seconds.toString(), 671/2, 130/2)
 
                 // 일 한글
                 ctx.font = timeFont
                 ctx.fillStyle = this.textColor;
-                ctx.fillText('일', 230/2, 145/2)
+                ctx.fillText('일', 230/2, 135/2)
                 // 시간 한글
                 ctx.font = timeFont
                 ctx.fillStyle = this.textColor;
-                ctx.fillText('시간', 397/2, 145/2)
+                ctx.fillText('시간', 397/2, 135/2)
                 // 분 한글
                 ctx.font = timeFont
                 ctx.fillStyle = this.textColor;
-                ctx.fillText('분', 578/2, 145/2)
+                ctx.fillText('분', 578/2, 135/2)
                 // 초 한글
                 ctx.font = timeFont
                 ctx.fillStyle = this.textColor;
-                ctx.fillText('초', 731/2, 145/2)
+                ctx.fillText('초', 731/2, 135/2)
 
                 // 제목
                 ctx.font = titleFont
                 ctx.fillStyle = this.textColor;
-                ctx.fillText('프로모션 종료까지', 450/2, 73/2)
+                ctx.fillText('프로모션 종료까지', 450/2, 60/2)
 
                 // 늦기전에 사러가기 버튼
                 ctx.fillStyle = '#FF007B';
-                roundRect(ctx, 325/2, 196/2, 250/2, 60/2, 10/2, true);
+                roundRect(ctx, 135, 90, 180, 40, 10/2, true);
 
                 // 늦기전에 사러가기
                 ctx.font = buttonFont
                 ctx.fillStyle = this.textColor;
-                ctx.fillText('늦기전에 사러가기', 450/2, 226/2)
+                ctx.fillText('늦기전에 사러가기', 450/2, 110)
 
                 // 왼쪽 빨강이
                 ctx.fillStyle = '#FF007B';
@@ -208,22 +178,39 @@ module.exports = {
                 ctx.lineTo(867/2,0)
                 ctx.fill();
 
-                // add finalised frame to the gif
                 enc.addFrame(ctx);
-                
-                // remove a second for the next loop
                 timeResult.subtract(1, 'seconds');
             }
         } else {
-            // Date has passed so only using a string
-            
-            // BG
-            ctx.fillStyle = this.bg;
+
+            ctx.fillStyle = '#221900';
             ctx.fillRect(0, 0, this.width, this.height);
-            
-            // Text
+
+            const titleFont = 'normal normal bold 26px arial'
+
+            // 제목
+            ctx.font = titleFont
             ctx.fillStyle = this.textColor;
-            ctx.fillText(timeResult, this.halfWidth, this.halfHeight);
+            ctx.fillText('종료된 프로모션입니다.', 450/2, 75)
+
+            // 왼쪽 빨강이
+            ctx.fillStyle = '#FF007B';
+            ctx.beginPath();
+            ctx.moveTo(0,0)
+            ctx.lineTo(0,300/2)
+            ctx.lineTo(33/2, 300/2)
+            ctx.lineTo(98/2,0)
+            ctx.fill();
+
+            // 오른쪽 빨강이
+            ctx.fillStyle = '#FF007B';
+            ctx.beginPath();
+            ctx.moveTo(900/2,0)
+            ctx.lineTo(900/2,300/2)
+            ctx.lineTo(802/2, 300/2)
+            ctx.lineTo(867/2,0)
+            ctx.fill();
+
             enc.addFrame(ctx);
         }
         
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.get('/', function(req, res) {
     const date = req.query.date
-    GifMaker.init(date || '2019-10-18',900, 300, 'ffffff', '000000', new Date().getTime().toString(), 30,(a)=>{
+    GifMaker.init(date || '2019-10-18',900, 300, 'ffffff', '000000', new Date().getTime().toString(), 60,(a)=>{
         res.sendFile(a)
     })
 });
